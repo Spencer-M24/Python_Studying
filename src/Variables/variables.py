04bdef7472e1e14aab2fc2141319d04f0782f240
Spencer Meredith <Smeredith1119@gmail.com>
2019-09-13 13:23:09 -0700

    Basics Of Python Variables

---
+++ src/Variables/variables.py
@@ -0,0 +1,14 @@
+# This section is about Varibles the basic
+
+#A variable is a reserved memory space, What does that mean? Simply It can hold any value. Then it is  assign to a term.
+#Now lets look at a visual example below.
+
+#Created a Variable name is age whos value is 14. So when ever I call age it will print out the number 14, because age is set to 14.
+
+age = 14 #age is set to 14  #name is age 14 is the value.
+age # this is the name varible
+14 # this is the output from typing age which holds the number 14 inside of age.
+
+
+
+
